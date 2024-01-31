e34377100825f2dd541efdf8de4ba564124a2e37
SkuLL <102400540+ProjectSkuLL@users.noreply.github.com>
2024-01-31 12:04:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-const instagrambtn = document.getElementById("instagram");
-instagrambtn.addEventListener("click",()=> {
-    window.location.href= "https://www.instagram.com/s.codes__007/";
-});
-
-const repoBtn = document.getElementById("repobtn");
-repoBtn.addEventListener("click",()=> {
-    window.location.href="https://github.com/ProjectSkuLL/s.codes_-007.github.io";
-});
-
-
 // navbartoggle
 const navToggleBtn = document.querySelector(".navtoggleBtn");
 const navbar = document.getElementById("navigationbar");
@@ -23,4 +12,21 @@ navToggleBtn.addEventListener("click", ()=> {
         navbar.setAttribute('data-hidden',true);
         document.getElementById("navtogglebtn").innerHTML = `<i class="fa fa-solid fa-bars"></i>`;
     }
+});
+
+// redirection to pages
+// to redirect to my instagram profile
+const instagram = document.querySelector(".fa-instagram");
+instagram.addEventListener("click", ()=>{
+    window.location.href="https://www.instagram.com/s.codes__007?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==";
+});
+// to redirect to my main github page
+const github = document.querySelector(".fa-github");
+github.addEventListener("click",()=>{
+    window.location.href="https://github.com/ProjectSkuLL";
+});
+// for current repository
+const repository = document.querySelector(".repository");
+repository.addEventListener("click", ()=>{
+    window.location.href="https://github.com/ProjectSkuLL/s.codes_-007.github.io";
 });
